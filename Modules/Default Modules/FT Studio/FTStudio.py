5befd5152904bbe1d75c8e5f7d538737aef51951
MattBrantley <Matthew_Brantley@baylor.edu>
2018-11-12 06:53:45 -0600

    11_11_18

--- Modules/Default Modules/FT Studio/FTStudio.py
+++ Modules/Default Modules/FT Studio/FTStudio.py
@@ -6,6 +6,7 @@ from src.Managers.HardwareManager.PacketMeasurements import AnalogWaveformMeasur
 from src.Managers.ModuleManager.ModuleResource import *
 from PyQt5.QtChart import *
 from functools import partial
+import scipy.constants as spc
 import numpy as np
 import os
 
@@ -66,8 +67,8 @@ class FTStudio(DSModule):
     def initFTContainer(self):
         self.FTContainer.setOrientation(Qt.Vertical)
 
-        self.spectrumWindow = measurementView()
-        self.transientWindow = measurementView()
+        self.spectrumWindow = spectrumView()
+        self.transientWindow = transientView()
 
         self.FTContainer.addWidget(self.spectrumWindow)
         self.FTContainer.addWidget(self.transientWindow)
@@ -82,8 +83,11 @@ class FTStudio(DSModule):
             self.transientWindow.clearData()
             self.transientWindow.addData(self.selectedPacket.Get_Measurement_Packet())
 
+            self.spectrumWindow.clearData()
+            self.spectrumWindow.addData(self.selectedPacket.Get_Measurement_Packet())
 
-class measurementView(QChartView):
+
+class spectrumView(QChartView):
     def __init__(self):
         super().__init__()
         self.initChart()
@@ -94,11 +98,11 @@ class measurementView(QChartView):
         self.setChart(self.chart)
 
         self.xValueAxis = QValueAxis()
-        self.xValueAxis.setTitleText('Time (s)')
+        self.xValueAxis.setTitleText('m/z')
         self.chart.addAxis(self.xValueAxis, Qt.AlignBottom)
 
         self.yValueAxis = QValueAxis()
-        self.yValueAxis.setTitleText('Voltage (V)')
+        self.yValueAxis.setTitleText('Intensity (arb)')
         self.chart.addAxis(self.yValueAxis, Qt.AlignLeft)
 
     def initView(self):
@@ -111,7 +115,14 @@ class measurementView(QChartView):
 
     def addData(self, packet, color=None):
         for measurement in packet.Get_Measurements():
-            self.createLine(measurement.xData(zeroOrigin=True), measurement.yData(), color=color)
+            xData, yData = self.doFFT(measurement.xData(zeroOrigin=True), measurement.stepSize())
+            self.createLine(xData, yData, color=color)
+            #self.createLine(measurement.xData(zeroOrigin=True), measurement.yData(), color=color)
+
+    def createSelectionRect(self):
+        rect = QRectF(1, 1, 1, 1)
+        rectItem = QGraphicsRectItem(rect)
+        self.chart.add
 
     def createLine(self, xdata, ydata, color=None):
         curve = QLineSeries()
@@ -138,7 +149,20 @@ class measurementView(QChartView):
         memory[:(size-1)*2+1:2] = xdata
         memory[1:(size-1)*2+2:2] = ydata
         return polyline        
-        
+
+    ##### MATHS #####
+    def doFFT(self, data, dT):
+        yData = np.fft.rfft(data)
+        yData = np.abs(yData)**2
+        xData = np.fft.rfftfreq(data.shape[0], dT)
+        vfunc = np.vectorize(self.ftomz)
+        xData = vfunc(xData[1:], 9.34)
+
+        return xData, yData[1:]
+
+    def ftomz(self, f, b):
+        return (spc.e*b) / (2*spc.pi*f) / 1.660539e-27
+
     def mousePressEvent(self, mouseEvent):
         if(mouseEvent.buttons() & Qt.RightButton):
             mouseEvent.accept()
@@ -151,6 +175,69 @@ class measurementView(QChartView):
             mouseEvent.accept()
         else:
             super().mouseReleaseEvent(mouseEvent)
+
+
+
+class transientView(QChartView):
+    def __init__(self):
+        super().__init__()
+        self.initChart()
+        self.initView()
+
+    def initChart(self):
+        self.chart = measurementChart()
+        self.setChart(self.chart)
+
+        self.xValueAxis = QValueAxis()
+        self.xValueAxis.setTitleText('Time (s)')
+        self.chart.addAxis(self.xValueAxis, Qt.AlignBottom)
+
+        self.yValueAxis = QValueAxis()
+        self.yValueAxis.setTitleText('Voltage (V)')
+        self.chart.addAxis(self.yValueAxis, Qt.AlignLeft)
+
+    def initView(self):
+        #self.setRubberBand(QChartView.HorizontalRubberBand)
+        self.setRenderHint(QPainter.Antialiasing)
+        self.setFrameStyle(QFrame.NoFrame)
+
+    def clearData(self):
+        self.chart.removeAllSeries()
+
+    def addData(self, packet, color=None):
+        for measurement in packet.Get_Measurements():
+            self.createLine(measurement.xData(zeroOrigin=True), measurement.yData(), color=color)
+
+    def createSelectionRect(self):
+        rect = QRectF(1, 1, 1, 1)
+        rectItem = QGraphicsRectItem(rect)
+        self.chart.add
+
+    def createLine(self, xdata, ydata, color=None):
+        curve = QLineSeries()
+        pen = curve.pen()
+        if color is not None:
+            pen.setColor(color)
+        pen.setWidthF(.1)
+        curve.setPen(pen)
+        curve.setUseOpenGL(True)
+        curve.append(self.series_to_polyline(xdata, ydata))
+        self.chart.addSeries(curve)
+
+        curve.attachAxis(self.xValueAxis)
+        curve.attachAxis(self.yValueAxis)
+        #self.chart.createDefaultAxes()
+
+    def series_to_polyline(self, xdata, ydata):
+        size = len(xdata)
+        polyline = QPolygonF(size)
+        pointer = polyline.data()
+        dtype, tinfo = np.float, np.finfo  # integers: = np.int, np.iinfo
+        pointer.setsize(2*polyline.size()*tinfo(dtype).dtype.itemsize)
+        memory = np.frombuffer(pointer, dtype)
+        memory[:(size-1)*2+1:2] = xdata
+        memory[1:(size-1)*2+2:2] = ydata
+        return polyline        
 
 class measurementChart(QChart):
     def __init__(self):
